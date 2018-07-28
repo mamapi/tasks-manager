573b9d08b5ddd11ff701bfabf056c514ddf3bb71
Gregory G <gorczynski.grzegorz@gmail.com>
2018-07-28 20:27:50 +0200

    Call register routes

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 
 const Hapi = require('hapi');
 
+const taskRoutes = require('./server/routes/routes.task')
+
 const server = new Hapi.server({
     host: 'localhost',
     port: 3000,
 });
 
+const registerRoutes = () => {
+    taskRoutes.registerRoutes(server)
+};
+
 const initServer = async () => {
+    await registerRoutes();
     await server.start();
     return server;
 };
