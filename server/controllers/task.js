d3f8aa234f4388dcea819721eafd89430df2c922
Gregory G <gorczynski.grzegorz@gmail.com>
2018-07-28 17:11:19 +0200

    Create Task controller

---
+++ server/controllers/task.js
@@ -0,0 +1,34 @@
+/**
+ * List tasks
+ */
+exports.list = (req, h) => {
+
+}
+
+/**
+ * Get task by ID
+ */
+exports.get = (req, h) => {
+
+}
+
+/**
+ * Create a task
+ */
+exports.create = (req, h) => {
+
+}
+
+/**
+ * Update task by ID
+ */
+exports.update = (req, h) => {
+
+}
+
+/**
+ * Delete task by ID
+ */
+exports.remove = (req, h) => {
+
+}
