6d0236ac4f168617bd97c61c9f050cda3e6b3fef
Gregory G <gorczynski.grzegorz@gmail.com>
2018-07-28 21:22:54 +0200

    Add input validation data

--- server/routes/routes.task.js
+++ server/routes/routes.task.js
@@ -1,39 +1,59 @@
+const Joi = require('joi')
+
 const TaskController = require('../controllers/task')
 
 exports.registerRoutes = (server) => {
-    
+
     return server.route([
         {
             method: 'GET',
             path: '/tasks',
-            options: {
-                handler: TaskController.list,
-            }
+            handler: TaskController.list,
         },
         {
             method: 'GET',
             path: '/tasks/{id}',
-            options: {
-                handler: TaskController.get
-            }
+            handler: TaskController.get
         },
         {
             method: 'POST',
             path: '/tasks',
+            handler: TaskController.create,
             options: {
-                handler: TaskController.create
+                validate: {
+                    payload: taskValidator
+                }
             }
         },
         {
             method: 'PUT',
             path: '/tasks/{id}',
-            handler: TaskController.update
+            handler: TaskController.update,
+            options: {
+                validate: {
+                    payload: taskValidator,
+                    params: {
+                        id: Joi.number().integer().min(1).required()
+                    }
+                }
+            }
         },
         {
             method: 'DELETE',
             path: '/tasks/{id}',
-            handler: TaskController.remove
+            handler: TaskController.remove,
+            options: {
+                validate: {
+                    params: {
+                        id: Joi.number().integer().min(1).required()
+                    }
+                }
+            }
         }
     ])
 };
 
+const taskValidator = {
+    'name': Joi.string().min(1).max(100).required(),
+    'description': Joi.string().max(255)
+}
