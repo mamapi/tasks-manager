613dd9038979f7520339c062a420341618005d32
Gregory G <gorczynski.grzegorz@gmail.com>
2018-07-28 17:11:40 +0200

    Add routes

---
+++ server/routes/routes.js
@@ -0,0 +1,37 @@
+const TaskController = require('./controllers/task');
+
+exports.registerRoutes = (server) => {
+    return server.route([
+        {
+            method: 'GET',
+            path: '/tasks',
+            options: {
+                handler: TaskController.list
+            }
+        },
+        {
+            method: 'GET',
+            path: '/tasks/{id}',
+            options: {
+                handler: TaskController.get
+            }
+        },
+        {
+            method: 'POST',
+            path: '/tasks',
+            options: {
+                handler: TaskController.create
+            }
+        },
+        {
+            method: 'PUT',
+            path: '/tasks/{id}',
+            handler: TaskController.update
+        },
+        {
+            method: 'DELETE',
+            path: '/tasks/{id}',
+            handler: TaskController.remove
+        }
+    ])
+};
