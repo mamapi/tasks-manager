2b88862377140e3a7f861ca7cb7b0894dac74090
Gregory G <gorczynski.grzegorz@gmail.com>
2018-07-28 21:22:15 +0200

    Change length of column in model

--- server/models/task.js
+++ server/models/task.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   var Task = sequelize.define('Task', {
     name: DataTypes.STRING,
-    description: DataTypes.STRING
+    description: DataTypes.STRING(100)
   }, {});
   Task.associate = function(models) {
     // associations can be defined here
