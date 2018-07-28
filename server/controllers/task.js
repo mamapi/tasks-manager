8964b17cf6be7e03b67d8d289481ac3e4589779e
Gregory G <gorczynski.grzegorz@gmail.com>
2018-07-28 20:23:54 +0200

    Impement CRUD operations

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -1,34 +1,47 @@
+const Task = require('../models').Task;
+
 /**
  * List tasks
  */
 exports.list = (req, h) => {
-
+    return Task.findAll()
 }
 
 /**
  * Get task by ID
  */
 exports.get = (req, h) => {
-
+    return Task.findById(req.params.id)
 }
 
 /**
  * Create a task
  */
 exports.create = (req, h) => {
-
+    const taskData = {
+        name: req.payload.name,
+        description: req.payload.description
+    };
+    return Task.create(taskData)
 }
 
 /**
  * Update task by ID
  */
-exports.update = (req, h) => {
-
+exports.update = async (req, h) => {
+    const id = req.params.id
+    const task = await Task.findById(id)
+    return task.update({
+        name: req.payload.name,
+        description: req.payload.description
+    });
 }
 
 /**
  * Delete task by ID
  */
-exports.remove = (req, h) => {
-
+exports.remove = async (req, h) => {
+    const id = req.params.id
+    const task = await Task.findById(id)
+    return task.remove();
 }
