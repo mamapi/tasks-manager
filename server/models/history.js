253080229b16bd83c4acd69fd0649fbc71d10e70
Gregory G <gorczynski.grzegorz@gmail.com>
2018-08-02 23:20:05 +0200

    Add freezeTableName option for history table

--- server/models/history.js
+++ server/models/history.js
@@ -3,7 +3,9 @@ module.exports = (sequelize, DataTypes) => {
   var History = sequelize.define('History', {
     taskId: DataTypes.INTEGER,
     description: DataTypes.STRING
-  }, {});
+  }, {
+      freezeTableName: true,
+    });
   History.associate = function (models) {
     // associations can be defined here
   };
