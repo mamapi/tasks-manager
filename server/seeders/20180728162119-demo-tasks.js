2e03af333627fc630cc8190ab26864688fbce4e2
Gregory G <gorczynski.grzegorz@gmail.com>
2018-08-03 11:19:13 +0200

    Update demo data

--- server/seeders/20180728162119-demo-tasks.js
+++ server/seeders/20180728162119-demo-tasks.js
@@ -4,21 +4,61 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Tasks', [
       {
-        name: 'Task 1 ',
+        name: 'Get conference tickets',
         description: '',
         createdAt: new Date(),
         updatedAt: new Date(),
         status: 'New'
       },
       {
-        name: 'Task 2',
+        name: 'Buy new office desk',
         description: '',
         createdAt: new Date(),
         updatedAt: new Date(),
         status: 'New'
       },
-
-    ], {});
+      {
+        name: 'Talk to Mike',
+        description: '',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        status: 'New'
+      },
+      {
+        name: 'Review contract with pr agency',
+        description: '',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        status: 'New'
+      },
+      {
+        name: 'Book venue',
+        description: 'Book venue for party',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        status: 'New'
+      },   
+      {
+        name: 'Call Mike',
+        description: 'Call Mike',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        status: 'New'
+      },         
+      {
+        name: 'Complete documentation',
+        description: 'Camplete project documentation',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        status: 'New'
+      },         
+      {
+        name: 'Prepare ad compaign',
+        description: 'Prepare ad compaign',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        status: 'New'
+      },     ], {});
   },
 
   down: (queryInterface, Sequelize) => {
