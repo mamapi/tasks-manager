48ffba0e94c250217a014aec6de44724787a48a9
Gregory G <gorczynski.grzegorz@gmail.com>
2018-08-06 19:42:25 +0200

    Add component TopMenu

---
+++ client/src/TopMenu.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+import {
+    Container,
+    Divider,
+    Dropdown,
+    Grid,
+    Header,
+    Image,
+    List,
+    Menu,
+    Segment,
+} from 'semantic-ui-react'
+
+const TopMenu = () => (
+            <Menu inverted>
+                <Container>
+                    <Menu.Item as='a' header>
+                        Tasks manager
+                </Menu.Item>
+                    <Menu.Item as={Link} to='/tasks'>Tasks</Menu.Item>
+                    <Menu.Item>MenuItem2</Menu.Item>
+
+                    <Dropdown item simple text='Dropdown'>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>List Item</Dropdown.Item>
+                            <Dropdown.Item>List Item</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Header>Header Item</Dropdown.Header>
+                            <Dropdown.Item>
+                                <i className='dropdown icon' />
+                                <span className='text'>Submenu</span>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item>List Item</Dropdown.Item>
+                                    <Dropdown.Item>List Item</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown.Item>
+                            <Dropdown.Item>List Item</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Container>
+            </Menu>
+
+)
+
+export default TopMenu
