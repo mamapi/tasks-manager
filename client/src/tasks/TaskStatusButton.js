a9549040e1a7c0cab360dbca4c403e5034d7209b
Gregory G <gorczynski.grzegorz@gmail.com>
2018-08-06 15:45:01 +0200

    Add statusLocal to state

--- client/src/tasks/TaskStatusButton.js
+++ client/src/tasks/TaskStatusButton.js
@@ -3,45 +3,52 @@ import axios from 'axios'
 import { Button } from 'react-bootstrap'
 
 class TaskStatusButton extends Component {
-
     constructor(props) {
         super(props)
 
         this.state = {
-            status: props.status
+            status: props.status,
+            statusLocal: props.statusLocal,
         }
-
-        this.markCompleted = this.updateStatus.bind(this);
     }
+
+    componentWillReceiveProps(nextProps) {
+        if(this.props !== nextProps){
+            this.setState({ 
+                status: nextProps.status,
+                statusLocal: nextProps.statusLocal,
+             })
+        }
+      }
 
     updateStatus(e, id, status) {
         e.preventDefault();
 
         axios.put(`/api/tasks/${id}/status`, { status })
             .then((result) => {
-                this.setState({ status: result.data.status })
+                this.setState({ 
+                    status: result.data.status,
+                    statusLocal: result.data.statusLocal,
+                 })
             }).catch((error) => console.error(error))
     }
 
     render() {
         let button
-        console.log('TaskStatusButton state:')
-        console.log(this.state)
-
         if (this.state.status === "Completed") {
             button =
                 <Button
                     bsStyle="success"
                     onClick={(e) => this.updateStatus(e, this.props.id, 'New')}
                     bsSize="small">
-                    {this.state.status}
+                    {this.state.statusLocal}
                 </Button>
         } else {
             button =
                 <Button
                     onClick={(e) => this.updateStatus(e, this.props.id, 'Completed')}
                     bsSize="small">
-                    {this.state.status}
+                    {this.state.statusLocal}
                 </Button>
         }
         return (
