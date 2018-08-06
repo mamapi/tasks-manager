c244e0419d07b9d1eae8441fef640003c6d572e0
Gregory G <gorczynski.grzegorz@gmail.com>
2018-08-06 15:31:53 +0200

    register hapi-i18n plugin

--- server.js
+++ server.js
@@ -3,21 +3,37 @@
 const Hapi = require('hapi');
 
 const taskRoutes = require('./server/routes/routes.task')
+const { sequelize } = require('./server/models');
+
 
 const server = new Hapi.Server({
     host: 'localhost',
     port: process.env.PORT || 3001,
 });
 
-const registerRoutes = () => {
+const registerRoutes = async () => {
     taskRoutes.registerRoutes(server)
 };
 
+const registerPlugins = async () => {
+    await server.register({
+        plugin: require('hapi-i18n'),
+        options: {
+            locales: ['pl', 'en', 'de'],
+            directory: __dirname + '/server//locales',
+            languageHeaderField: 'language',
+            defaultLocale: 'en',
+        }
+    })
+}
+
 const initServer = async () => {
-    await registerRoutes();
-    await server.start();
-    return server;
-};
+    await sequelize.sync()
+    await registerPlugins()
+    await registerRoutes()
+    await server.start()
+    return server
+}
 
 initServer().then(server => {
     console.log(`Server running at: ${server.info.uri}`);
